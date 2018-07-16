cf5cc1fe028392e2dc43789a4f0b8b5e144418fb
Neeraj1619 <neerajkmr1619@gmail.com>
2018-07-16 18:48:45 +0530

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count (int min ,int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+
+        public int columns = 8;                                         //Number of columns in our game board.
+        public int rows = 8;                                            //Number of rows in our game board.
+        public Count wallCount = new Count(5, 9);                      //Lower and upper limit for our random number of walls per level.
+        public Count foodCount = new Count(1, 5);                      //Lower and upper limit for our random number of food items per level.
+        public GameObject exit;                                         //Prefab to spawn for exit.
+        public GameObject[] floorTiles;                                 //Array of floor prefabs.
+        public GameObject[] wallTiles;                                  //Array of wall prefabs.
+        public GameObject[] foodTiles;                                  //Array of food prefabs.
+        public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
+        public GameObject[] outerWallTiles;
+
+        private Transform boardHolder;
+        private List<Vector3>gridPositions = new List<Vector3>();
+    }
+}
